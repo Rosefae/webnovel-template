5d45d47f0d41dbb8618f0e86fb76dc22cb87fa20
Yiyi Rose Liu <rliu@gorillagroup.com>
2019-03-27 16:47:53 -0400

    Header partial, arabic numeral to roman numeral filter

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,15 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy("src/_scripts");
     eleventyConfig.addPassthroughCopy("src/_assets");
 
+    eleventyConfig.addFilter("romanNumerals", function(n){
+        let roman = arabicToRoman(n);
+        return roman;
+    });
+    eleventyConfig.addFilter("romanNumeralsAccessible", function(n){
+        let roman = arabicToRoman(n);
+        return `<span aria-label="${n}"><span aria-hidden="true">${roman}</span></span>`;
+    });
+
     return {
         dir: {
             input: "src",
@@ -10,5 +19,53 @@ module.exports = function(eleventyConfig){
         },
         passthroughFileCopy: true
     }
+}
 
+function arabicToRoman(n){
+    var numberSymbols = [
+        {
+            "one" : "I",
+            "five" : "V",
+        },
+        {
+            "one" : "X",
+            "five" : "L",
+        },
+        {
+            "one" : "C",
+            "five" : "D",
+        },
+        {
+            "one" : "M"
+        }
+    ];
+    var output="";
+    if(n>3999 || n<=0){
+        return n;
+    }
+    var s = n.toString();
+    var place = 0;
+    for (i=s.length-1; i>=0; i--){
+        let d = Number(s[i]);
+        let r = "";
+        switch(d){
+            case 9:
+                r = numberSymbols[place].one + numberSymbols[place+1].one;
+                break;
+            case 4:
+                r = numberSymbols[place].one + numberSymbols[place].five;
+                break;
+            default:
+                if (d>=5){
+                    r = numberSymbols[place].five;
+                    d = d-5;
+                }
+                for(j=0;j<d;j++){
+                    r += numberSymbols[place].one;
+                }
+        }
+        output = r + output;
+        place++;
+    }
+    return output;
 }
--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A template for webnovels using Eleventy.
 
 `npm start` and `localhost:8080`.
 
+Using Liquid for the layout just because the markdown is pre-processed with Liquid by default anyway.
+
 ## scripts
 
 `serve`: Runs `eleventy --serve`. Compiles templates in `src` into `dist`, as well as copying over `_assets` and `_scripts` as-is. Also watches and serves the site to `localhost:8080`.
