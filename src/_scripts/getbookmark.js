87d1b79c8669628224dbda5b7298ed08bf8470e8
Yiyi Rose Liu <rliu@gorillagroup.com>
2020-05-08 00:25:23 -0400

    Changes to how styles / scripts are served

--- src/_scripts/getbookmark.js
+++
@@ -1,7 +0,0 @@
-let currChapter = localStorage.getItem("currChapter");
-let currChapterLink = document.getElementById("currChapterLink");
-
-if (currChapter){
-    currChapterLink.firstElementChild.setAttribute("href", "read/" + currChapter + "/");
-    currChapterLink.removeAttribute("hidden");
-}
